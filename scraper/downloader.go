0fc8884be096fc67314ff8d3cf5f16af60fb253e
Zhiwen Shen <weidaru@gmail.com>
2014-03-03 14:28:01 -0800

    Added some placeholder functions to downloader.

--- scraper/downloader.go
+++ scraper/downloader.go
@@ -1,1 +1,38 @@
 package scraper
+
+type Downloader struct {
+
+}
+
+
+func (d *Downloader) New(log_filename string) *Downloader {
+	return nil
+}
+
+func (d *Downloader) Request(url string) {
+
+}
+
+func (d *Downloader) SetCheckpointInterval(interval int) {
+
+}
+
+func (d *Downloader) Execute() {
+
+}
+
+func (d *downloader) Resume() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
